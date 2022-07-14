ccc6bc9e677d11311c061f6d36b3f77f25030416
Edward Tanguay (windows) <edward@tanguay.info>
2022-07-14 08:16:08 +0200

    login finished

--- server.js
+++ server.js
@@ -7,13 +7,6 @@ import cors from 'cors';
 import jwt from 'jsonwebtoken';
 import bcrypt from 'bcrypt';
 
-const user = {
-	id: 1,
-	username: 'hans',
-	firstName: 'Hans',
-	lastName: 'Richter',
-};
-
 dotenv.config();
 
 const MONGODB_URI =
